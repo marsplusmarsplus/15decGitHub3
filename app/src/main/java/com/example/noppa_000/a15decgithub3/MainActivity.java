336c764fb017687ea614f9e1025c758ef43f9266
marsplusmarsplus <marsplus@hotmail.com>
2017-12-15 05:39:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/noppa_000/a15decgithub3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.noppa_000.a15decgithub3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
